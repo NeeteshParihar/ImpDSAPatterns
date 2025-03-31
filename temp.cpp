d7c17580125986b01f1070242fc4cad5599993c8
NeeteshParihar <neeteshparihar22@gmail.com>
2025-04-01 02:50:31 +0530

    add catalan number pattern

--- temp.cpp
+++ temp.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+
+using namespace std ;
+
+
+int main(){
+
+	int price   ;
+	int margin  ; 
+	int rto     ; 
+
+	cout << "price :"  ;
+	cin >> price ;
+
+	cout << "rto :" ;
+	cin >> rto ;
+
+	price += rto ;
+
+	cout << "margin :" << " : " ;
+	cin >> margin ;
+
+	int profit = (margin*price)/100   ;
+	int finalMargin = profit + price ;
+
+	cout << " finalPrice : " << finalMargin << endl ;
+
+	main() ;
+
+	
+
+}
